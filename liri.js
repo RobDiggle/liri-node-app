3d6b7c2de195d6c411e853639c6c0e8daa1a795b
Robert Diggle <Robschach13@yahoo.com>
2017-04-01 20:54:25 -0400

    3rd commit

--- liri.js
+++ liri.js
@@ -13,6 +13,7 @@
 var keys = require('./keys.js');
 var twitter = require('twitter');
 var twitterKeys = keys.twitterKeys;
+var spotify = require('spotify');
 
 var client = new twitter({
         consumer_key: keys.twitterKeys.consumer_key,
@@ -36,17 +37,24 @@ function myTweets() {
     });
 }
 
+var spotifyObtain = function (songName){
 
-var spotify = require('spotify');
  
-spotify.search({ type: 'track', query: 'we belong' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
+function spotify(){
+    spotify.search({ type: 'track', query: songName }, function(error, data) {
+    if ( error ) {
+        console.log('Error occurred: ' + error);
         return;
     }
+    else if( !error)
+    {
+        console.log( + " is a great song.")
+    }
  
-    // Do something with 'data' 
-});
+    console.log(data.tracks.items[0])
+    });
+}
+}
 
 myTweets();
 
