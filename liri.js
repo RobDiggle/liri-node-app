5c627be368a5ab3f2e2ab9886797b76aa5f0b43a
Robert Diggle <Robschach13@yahoo.com>
2017-04-01 21:18:46 -0400

    4th commit

--- liri.js
+++ liri.js
@@ -37,10 +37,14 @@ function myTweets() {
     });
 }
 
+var artistCall = function(artist) {
+    return artist.name;
+}
+
 var spotifyObtain = function (songName){
 
  
-function spotify(){
+
     spotify.search({ type: 'track', query: songName }, function(error, data) {
     if ( error ) {
         console.log('Error occurred: ' + error);
@@ -49,13 +53,66 @@ function spotify(){
     else if( !error)
     {
         console.log( + " is a great song.")
-    }
+    
  
-    console.log(data.tracks.items[0])
-    });
-}
+    var songs = data.tracks.items;
+    for(i=0; i < songs.length; i++)
+    {
+         console.log(i);
+
+    }
+
+    console.log(songs[i].name)
+    }});
+
 }
 
+var omdb = require('omdb');
+ 
+var findMovie = function(movieName) {
+    omdb.search('saw', function(err, movies) {
+    if(err) {
+        return console.error(err);
+    }
+ 
+    if(movies.length < 1) {
+        return console.log('No movies were found!');
+    }
+ 
+    movies.forEach(function(movie) {
+        console.log('%s (%d)', movie.title, movie.year);
+    });
+ 
+    // Saw (2004) 
+    // Saw II (2005) 
+    // Saw III (2006) 
+    // Saw IV (2007) 
+    // ... 
+});
+ 
+omdb.get({ title: 'Saw', year: 2004 }, true, function(err, movie) {
+    if(err) {
+        return console.error(err);
+    }
+ 
+    if(!movie) {
+        return console.log('Movie not found!');
+    }
+ 
+    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    console.log(movie.plot);
+ 
+    // Saw (2004) 7.6/10 
+    // Two men wake up at opposite sides of a dirty, disused bathroom, chained 
+    // by their ankles to pipes. Between them lies... 
+}});
+
+
+
+
+
+
 myTweets();
+spotifyObtain();
 
 //
